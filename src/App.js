3d937abbd2df58c22116f0e11852ae5c8016342c
LAPTOP-Q462S81B\ASUS <doantrungminh20@gmail.com>
2024-10-05 15:57:35 +0700

    Lab3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-import { Badge, Button, Card, Carousel, Col, Container, Form, Modal, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Button, Card, Carousel, Col, Container, Modal, Row } from 'react-bootstrap';
 import './App.css';
+import Header from './components/Header';
+import SelectItemsPopup from "./components/SelectItemsPopup";
 import menu1 from './images/menu1.jpg';
 import menu2 from './images/menu2.jpg';
 import menu3 from './images/menu3.jpg';
@@ -11,158 +13,144 @@ import pizza2 from './images/pizza2.jpg';
 import pizza3 from './images/pizza3.jpg';
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [cart, setCart] = useState({});
+  const [totalItems, setTotalItems] = useState(0);
+  const [showPopup, setShowPopup] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    const storedCart = JSON.parse(localStorage.getItem("cart")) || {};
+    setCart(storedCart);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+    setTotalItems(
+      Object.values(cart).reduce((sum, item) => sum + item.count, 0)
+    );
+  }, [cart]);
+
+  const addToCart = (item) => {
+    const newCart = { ...cart };
+    if (newCart[item.id]) {
+      newCart[item.id].count += 1;
+    } else {
+      newCart[item.id] = { ...item, count: 1 };
+    }
+    setCart(newCart);
+  };
+
+  const handleQuantityChange = (id, delta) => {
+    const newCart = { ...cart };
+    newCart[id].count += delta;
+    if (newCart[id].count <= 0) {
+      delete newCart[id];
+    }
+    setCart(newCart);
+  };
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
   return (
     <Container>
-      <Row>
-        <Navbar expand="lg" className="bg-body-tertiary">
-          <Container fluid>
-            <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-              >
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Link" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#" disabled>
-                  Link
-                </Nav.Link>
-              </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-              <Button variant="primary" onClick={handleShow}>
-                Profile <Badge bg="secondary">9</Badge>
-                <span className="visually-hidden">unread messages</span>
-              </Button>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </Row>
+
+      <Header handleShow={togglePopup} totalItems={totalItems} />
+
+
       <Row>
         <Carousel>
           <Carousel.Item>
-            <img src={pizza1} />
+            <img src={pizza1} alt="Pizza 1" />
             <Carousel.Caption>
               <h3>First Pizza</h3>
               <p>Fruit Pizza</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img src={pizza2} />
+            <img src={pizza2} alt="Pizza 2" />
             <Carousel.Caption>
               <h3>Second Pizza</h3>
               <p>Vegan Pizza</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img src={pizza3} />
+            <img src={pizza3} alt="Pizza 3" />
             <Carousel.Caption>
               <h3>Third Pizza</h3>
-              <p>
-                Cheese Pizza
-              </p>
+              <p>Cheese Pizza</p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </Row>
+
+
       <Row className='mt-3'>
         <Col>
           <Card>
             <Card.Img variant="top" src={menu1} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <Card.Title>Margherita Pizza</Card.Title>
+              <Card.Text>$24.00</Card.Text>
+              <Button variant="primary" onClick={() => addToCart({ id: 1, title: 'Margherita Pizza', price: 24.00 })}>
+                Buy
+              </Button>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card >
+          <Card>
             <Card.Img variant="top" src={menu2} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <Card.Title>Mushroom Pizza</Card.Title>
+              <Card.Text>$25.00</Card.Text>
+              <Button variant="primary" onClick={() => addToCart({ id: 2, title: 'Mushroom Pizza', price: 25.00 })}>
+                Buy
+              </Button>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card >
+          <Card>
             <Card.Img variant="top" src={menu3} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <Card.Title>Hawaiian Pizza</Card.Title>
+              <Card.Text>$30.00</Card.Text>
+              <Button variant="primary" onClick={() => addToCart({ id: 3, title: 'Hawaiian Pizza', price: 30.00 })}>
+                Buy
+              </Button>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card >
+          <Card>
             <Card.Img variant="top" src={menu4} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <Card.Title>Pesto Pizza</Card.Title>
+              <Card.Text>$30.00</Card.Text>
+              <Button variant="primary" onClick={() => addToCart({ id: 4, title: 'Pesto Pizza', price: 30.00 })}>
+                Buy
+              </Button>
             </Card.Body>
           </Card>
         </Col>
       </Row>
-      <Row>
-       
 
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </Row>
+
+      <Modal show={showPopup} onHide={togglePopup}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SelectItemsPopup cart={cart} onClose={togglePopup} onQuantityChange={handleQuantityChange} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={togglePopup}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
-
   );
 }
 
 export default App;
+
